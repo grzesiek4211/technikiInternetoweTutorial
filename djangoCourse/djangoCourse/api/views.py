f6b410086be7622b02d70db8b207bb85c9e33300
grzesiek4211 <grzesiek4211@gmail.com>
2019-11-11 20:41:13 +0100

    limit information from getAllMovies

--- djangoCourse/djangoCourse/api/views.py
+++ djangoCourse/djangoCourse/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
-from .serializers import MovieSerializer
+from rest_framework.response import Response
+
+from .serializers import MovieSerializer, MovieMiniSerializer
 from .models import Movie
 
 
@@ -9,3 +11,8 @@ class MovieViewSet(viewsets.ModelViewSet):
     """
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    def list(self, request, *args, **kwargs):
+        movies = Movie.objects.all()
+        serializer = MovieMiniSerializer(movies, many=True)
+        return Response(serializer.data)
